1fa657291faa97ede5ff018258b6a5f707460629
yanagiragi <qaz90473@gmail.com>
2018-01-08 21:12:22 +0800

    remove intensity

--- shaders/Debug.fs
+++ shaders/Debug.fs
@@ -1,11 +1,11 @@
 #version 440
-uniform float Intensity;
-uniform vec3 debugColor;
 
+uniform vec3 debugColor;
+uniform float Intensity;
 out vec4 outColor;
 
 void main() 
 {
-
 	outColor = vec4(debugColor , 1.0);
+	// outColor = vec4(debugColor , 1.0) * Intensity;
 }
